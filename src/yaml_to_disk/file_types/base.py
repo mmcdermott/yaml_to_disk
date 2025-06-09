3c681bc60f05d8c880345b0fbc616745cc555feb
Matthew McDermott <mattmcdermott8@gmail.com>
2025-06-09 23:20:16 +0800

    Fix pre-commit issues

--- src/yaml_to_disk/file_types/base.py
+++ src/yaml_to_disk/file_types/base.py
@@ -22,10 +22,7 @@ class FileType(abc.ABC):
             True if the file path matches the expected file type, False otherwise.
         """
 
-        if isinstance(cls.extension, (set, frozenset)):
-            matching_extensions = cls.extension
-        else:
-            matching_extensions = {cls.extension}
+        matching_extensions = cls.extension if isinstance(cls.extension, set | frozenset) else {cls.extension}
         return any("".join(file_path.suffixes) == ext for ext in matching_extensions)
 
     @classmethod
